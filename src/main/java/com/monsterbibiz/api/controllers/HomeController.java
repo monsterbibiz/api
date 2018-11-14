c3a5bb6ec0ff678c014ad80201f11740c9e6bd21
Ricardo Mora <monsterbibiz@gmail.com>
2018-11-13 22:25:07 -0500

    added index endpoint

--- src/main/java/com/monsterbibiz/api/controllers/HomeController.java
+++ src/main/java/com/monsterbibiz/api/controllers/HomeController.java
@@ -1,7 +1,13 @@
 package com.monsterbibiz.api.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
+  @RequestMapping("/")
+  public String index(){
+    return "caca";
+  }
 }
